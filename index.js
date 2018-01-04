9679f54e1c2952fbd9382ae889460b6a220f514e
Szewah Chin <swgrleong@gmail.com>
2018-01-04 22:07:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-    delete object[key];
+  delete object[key];
   return object;
 }
 
