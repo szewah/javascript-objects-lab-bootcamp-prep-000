636180c9be2346443e51ecad0dc7c279be04adb1
Szewah Chin <swgrleong@gmail.com>
2018-01-04 21:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var recipes = {}
 var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
 return Object.assign({},obj,{ [key]: value] })
-       Object.assign({},obj,{[key]:value})
-{ [key]: value }
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key,
