074d834f1dd984d7b68190f768ed5cf4d70d3ac3
Szewah Chin <swgrleong@gmail.com>
2018-01-04 21:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 return Object.assign({},object, key, value)
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key,
-value)
+value) {
+  
+}
