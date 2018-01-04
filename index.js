3c43d70aa2346e0dd5098c75c783e522d0ae05d0
Szewah Chin <swgrleong@gmail.com>
2018-01-04 21:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
 return Object.assign({},object, key, value)
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key,
+function destructivelyUpdateObjectWithKeyAndValue (object, key,
 value)
