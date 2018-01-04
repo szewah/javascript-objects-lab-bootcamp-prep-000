688dec163c539b7d21074f3461ec02b89e58d25f
Szewah Chin <swgrleong@gmail.com>
2018-01-04 22:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
+
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
