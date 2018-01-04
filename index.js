3f7216df0a9ffb9d6a0d099974945829e1a9a75b
Szewah Chin <swgrleong@gmail.com>
2018-01-04 21:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {}
 var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
-return Object.assign({},obj, {[key: value]})
+return Object.assign({},obj,{[key: value]})
+       Object.assign({},obj,{[key]:value})
 
 }
 
